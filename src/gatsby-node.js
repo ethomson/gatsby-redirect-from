590517373d6a0e5d4c56e948f5a8afa3134ad153
Edward Thomson <ethomson@edwardthomson.com>
2020-09-18 12:50:28 +0100

    destination: generate instead of using slug
    Our gatsby site is mdx and lacks the slug field in GraphQL.  Generate
    the destination instead.

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,67 +1,86 @@
-import chalk from 'chalk'
+"use strict";
 
-export function createPages({ graphql, actions }, pluginOptions) {
-  const { createRedirect } = actions
+const path = require('path');
 
-  const markdownQuery = pluginOptions.query || 'allMarkdownRemark'
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.createPages = createPages;
 
+var _chalk = _interopRequireDefault(require("chalk"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function createPages({
+  graphql,
+  actions
+}, pluginOptions) {
+  const {
+    createRedirect
+  } = actions;
+  const markdownQuery = pluginOptions.query || 'allMarkdownRemark';
   return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
+    resolve(graphql(`
           {
             q: ${markdownQuery}(
               filter: { frontmatter: { redirect_from: { ne: null } } }
             ) {
               edges {
                 node {
-                  fields {
-                    slug
-                  }
                   frontmatter {
                     redirect_from
+                  }
+                  parent {
+                    ... on File {
+                      relativeDirectory
+                      name
+                    }
                   }
                 }
               }
             }
           }
-        `
-      ).then((result) => {
-        if (result.errors) {
-          console.log(result.errors) // eslint-disable-line no-console
-          reject(result.errors)
-        }
+        `).then(result => {
+      if (result.errors) {
+        console.log(result.errors); // eslint-disable-line no-console
 
-        const allPosts = result.data.q.edges
+        reject(result.errors);
+      }
 
-        const redirects = []
+      const allPosts = result.data.q.edges;
+      const redirects = []; // For all posts with redirect_from frontmatter,
+      // extract all values and push to redirects array
 
-        // For all posts with redirect_from frontmatter,
-        // extract all values and push to redirects array
-        allPosts.forEach((post) => {
-          redirects.push({
-            from: post.node.frontmatter.redirect_from,
-            to: post.node.fields.slug
-          })
-        })
+      allPosts.forEach(post => {
+        const toPath = '/' + path
+          .join(
+            post.node.parent.relativeDirectory,
+            post.node.parent.name === 'index' ? '/' : post.node.parent.name,
+          )
+          .replace(/\\/g, '/') // Convert Windows backslash paths to forward slash paths: foo\\bar → foo/bar
 
-        // Create redirects from the just constructed array
-        redirects.forEach(({ from, to }) => {
-          // iterate through all `from` array items
-          from.forEach((from) => {
-            createRedirect({
-              fromPath: from,
-              toPath: to,
-              isPermanent: true,
-              redirectInBrowser: true
-            })
-          })
-        })
+        redirects.push({
+          from: post.node.frontmatter.redirect_from,
+          to: toPath
+        });
+      }); // Create redirects from the just constructed array
 
-        resolve(
-          console.log(`${chalk.green('success')} create redirects`) // eslint-disable-line no-console
-        )
-      })
-    )
-  })
+      redirects.forEach(({
+        from,
+        to
+      }) => {
+        // iterate through all `from` array items
+        from.forEach(from => {
+          createRedirect({
+            fromPath: from,
+            toPath: to,
+            isPermanent: true,
+            redirectInBrowser: true
+          });
+        });
+      });
+      resolve(console.log(`${_chalk.default.green('success')} create redirects`) // eslint-disable-line no-console
+      );
+    }));
+  });
 }
